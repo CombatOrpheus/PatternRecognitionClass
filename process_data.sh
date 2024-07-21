d55ac2fa863506081e241a6be3b352689eea64f4
Gabriel Santos Oliveira <santosgabriel28@hotmail.com>
2024-07-21 18:59:36 -0300

    Improvements to the pre-processing script
    There were some clarity issues with the original script, so now I
    also print out the names of the generated files while executing
    the script.

--- process_data.sh
+++ process_data.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 for file in $(find ./Data -name '*json'); do
-	name="$(basename $file | awk -F. '{print $1}')_line_oriented.json"
+	name="$(basename -s .json $file)"
 	path=$(dirname "$file")
-	echo "$name" "$path"
-	jq -c '.[]' "$file" > "$path/$name"
+	read -ra parts <<< "$(echo $file | awk -F/ '{print $3,  $4}')"
+	new_name="Data/${parts[0]}_${parts[1]}.processed"
+	set -x
+	jq -c '.[]' "$file" > "$new_name"
+	{ set +x; } &> /dev/null
+	echo "File written to $new_name"
 done

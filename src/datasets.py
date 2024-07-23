1114f6d9a1b38aaa8f30f5976363247fc4238ed0
Gabriel Santos Oliveira <santosgabriel28@hotmail.com>
2024-07-22 21:36:05 -0300

    Implementing basic DGLDataset
    Since I am having some problems with PyGeometric, having a branch
    with DGL is a good idea. This basic implementation is already
    working in the sense that it reads the data correctly, but I have
    not tested the learning process with it, nor have I implemented the
    padding of features for the dataset.

--- src/datasets.py
+++ src/datasets.py
@@ -1,20 +1,51 @@
-from torch_geometric.data import Data
-from torch_geometric.loader import DataLoader
+from itertools import starmap
 from pathlib import Path
-from data_parser import get_reachability_graphs
 from typing import List
+
+import dgl
 import numpy as np
+import torch
+from dgl.data import DGLDataset
+
+from src.data_parser import get_reachability_graphs
 
 
-def __reachability_to_data__(reachability_graph: List[np.array], label: float) -> Data:
-    return Data(
-        x=reachability_graph[0],
-        edge_index=reachability_graph[1],
-        edge_attr=reachability_graph[3],
-        y=label)
+class SPNDataset(DGLDataset):
+    def __init__(self):
+        super(SPNDataset, self).__init__(
+            name="Stochastic Petri Nets"
+        )
 
+    def process(self):
+        file = Path('Data/GridData_DS3.processed')
+        graphs = get_reachability_graphs(file)
+        self._labels = []
+        self._graphs = []
 
-def get_reachability_dataset(source: Path, batch_size=32) -> DataLoader:
-    iterator = get_reachability_graphs(source)
-    data = list(map(__reachability_to_data__, iterator))
-    return DataLoader(data, batch_size)
+        labels = []
+        for graph, label in graphs:
+            g = dgl.graph((graph[1][:, 0], graph[1][:, 1]), num_nodes=len(graph[0]))
+            g.ndata['feat'] = torch.tensor(graph[0]).float()
+            g.edata['feat'] = torch.tensor(graph[3][graph[2]]).view(-1, 1).float()
+
+            self._graphs.append(g)
+            labels.append(label)
+        self._labels = torch.tensor(labels).float()
+
+    def __getitem__(self, idx):
+        """ Get graph and label by index
+
+        Parameters
+        ----------
+        idx : int
+            Item index
+
+        Returns
+        -------
+        (dgl.DGLGraph, Tensor)
+        """
+        return self._graphs[idx], self._labels[idx]
+
+    def __len__(self):
+        """Number of graphs in the dataset"""
+        return len(self._graphs)

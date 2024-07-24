84cc227ae4a3cbea04fde0ea95f5240d77055717
Gabriel Santos Oliveira <santosgabriel28@hotmail.com>
2024-07-24 17:13:58 -0300

    Implementing basic functions to handle data
    Besides reading the data, I also added functions to discard node features and
    pad the data, althoug padding is being a little more difficult than I remember.
    I will also include a function to reduce the number of features per node later.

--- src/datasets.py
+++ src/datasets.py
@@ -1,20 +1,76 @@
+from itertools import starmap
+from pathlib import Path
+from typing import List, Iterator
+
+import numpy as np
+from torch import tensor
 from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader
-from pathlib import Path
-from data_parser import get_reachability_graphs
-from typing import List
-import numpy as np
+
+from .data_parser import get_reachability_graphs
 
 
-def __reachability_to_data__(reachability_graph: List[np.array], label: float) -> Data:
+def __pad_data__(iterator: Iterator) -> Iterator:
+    graph_info = []
+    labels = []
+    for data in iterator:
+        graph_info.append(data[0])
+        labels.append(data[1])
+
+    sizes = [net[0].shape for net in graph_info]
+    max_size = max((size[1] for size in sizes))
+    size_to_pad = ((size[0], max_size-size[1]) for size in sizes)
+    padding = (np.zeros(size) for size in size_to_pad)
+    graphs = (info[0] for info in graph_info)
+    pair = zip(graphs, padding)
+    padded_nets = [np.hstack((net, padding)) for net, padding in pair]
+    new_info = ([padded_nets, *info[1:]] for info in graph_info)
+    print(next(new_info))
+    return zip(new_info, labels)
+
+
+def __reduce_node_features__(
+        graph_info: List[np.array],
+        label: float
+        ) -> (List[np.array], float):
+    features = np.sum(graph_info[0], axis=1)
+    return ([features, *graph_info[1:]], label)
+
+
+def __to_data_no_node_features__(
+        graph_info: List[np.array],
+        label: float
+        ) -> Data:
     return Data(
-        x=reachability_graph[0],
-        edge_index=reachability_graph[1],
-        edge_attr=reachability_graph[3],
-        y=label)
+        edge_index=tensor(graph_info[1]).long(),
+        edge_attr=graph_info[2][graph_info[3]],
+        y=tensor(label))
 
 
-def get_reachability_dataset(source: Path, batch_size=32) -> DataLoader:
+def __to_data__(
+        graph_info: List[np.array],
+        label: float
+        ) -> Data:
+    return Data(
+        x=tensor(graph_info[0]),
+        edge_index=tensor(graph_info[1]),
+        edge_attr=tensor(graph_info[3]),
+        y=tensor(label))
+
+
+def get_reachability_dataset(
+        source: Path,
+        batch_size=32,
+        features: bool = True,
+        pad_data: bool = True
+        ) -> DataLoader:
     iterator = get_reachability_graphs(source)
-    data = list(map(__reachability_to_data__, iterator))
+    f = __to_data__ if features else __to_data_no_node_features__
+
+    if pad_data and features:
+        iterator = __pad_data__(iterator)
+    if not pad_data and features:
+        iterator = starmap(__reduce_node_features__, iterator)
+
+    data = list(starmap(f, iterator))
     return DataLoader(data, batch_size)

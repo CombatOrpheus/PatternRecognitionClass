8efd435238fb50e9a84a1662fa6ddd5672afde25
Gabriel Santos Oliveira <santosgabriel28@hotmail.com>
2024-07-25 16:26:36 -0300

    Simplification of dataset creation
    Instead of padding the node features so that all have the same
    size, I can just reduce the number to a scalar. I have alredy done
    some simple tests and checked that, indeed, the data loader works;
    the issue now is in loading the correct number of nodes. The
    solutions are either implementing a Dataset so that I can use a
    node sampler through PyG or pad the graphs with empty nodes.

--- src/datasets.py
+++ src/datasets.py
@@ -1,6 +1,6 @@
 from itertools import starmap
 from pathlib import Path
-from typing import List, Iterator
+from typing import List, Iterable
 
 import numpy as np
 from torch import tensor
@@ -10,67 +10,31 @@ from torch_geometric.loader import DataLoader
 from .data_parser import get_reachability_graphs
 
 
-def __pad_data__(iterator: Iterator) -> Iterator:
-    graph_info = []
-    labels = []
-    for data in iterator:
-        graph_info.append(data[0])
-        labels.append(data[1])
-
-    sizes = [net[0].shape for net in graph_info]
-    max_size = max((size[1] for size in sizes))
-    size_to_pad = ((size[0], max_size-size[1]) for size in sizes)
-    padding = (np.zeros(size) for size in size_to_pad)
-    graphs = (info[0] for info in graph_info)
-    pair = zip(graphs, padding)
-    padded_nets = [np.hstack((net, padding)) for net, padding in pair]
-    new_info = ([padded_nets, *info[1:]] for info in graph_info)
-    print(next(new_info))
-    return zip(new_info, labels)
+def __pad_node_features__(data: Iterable):
+    pass
 
 
-def __reduce_node_features__(
+def __to_data_reduced_features__(
         graph_info: List[np.array],
         label: float
-        ) -> (List[np.array], float):
-    features = np.sum(graph_info[0], axis=1)
-    return ([features, *graph_info[1:]], label)
-
-
-def __to_data_no_node_features__(
-        graph_info: List[np.array],
-        label: float
-        ) -> Data:
+) -> Data:
     return Data(
-        edge_index=tensor(graph_info[1]).long(),
-        edge_attr=graph_info[2][graph_info[3]],
-        y=tensor(label))
-
-
-def __to_data__(
-        graph_info: List[np.array],
-        label: float
-        ) -> Data:
-    return Data(
-        x=tensor(graph_info[0]),
-        edge_index=tensor(graph_info[1]),
-        edge_attr=tensor(graph_info[3]),
-        y=tensor(label))
+        x=tensor(np.sum(graph_info[0], axis=1)).view(-1, 1).float(),
+        edge_index=tensor(graph_info[1]).view(2, -1).long(),
+        edge_attr=tensor(graph_info[3][graph_info[2]]).float(),
+        y=tensor(label),
+        num_nodes=graph_info[0].shape[0])
 
 
 def get_reachability_dataset(
-        source: Path,
-        batch_size=32,
-        features: bool = True,
-        pad_data: bool = True
-        ) -> DataLoader:
-    iterator = get_reachability_graphs(source)
-    f = __to_data__ if features else __to_data_no_node_features__
+    source: Path,
+    reduce_node_features: bool = True,
+    batch_size=16
+) -> DataLoader:
+    data = get_reachability_graphs(source)
+    if reduce_node_features:
+        data_iterator = list(starmap(__to_data_reduced_features__, data))
+    else:
+        data_iterator = __pad_node_features__(data)
 
-    if pad_data and features:
-        iterator = __pad_data__(iterator)
-    if not pad_data and features:
-        iterator = starmap(__reduce_node_features__, iterator)
-
-    data = list(starmap(f, iterator))
-    return DataLoader(data, batch_size)
+    return DataLoader(data_iterator, batch_size=batch_size)

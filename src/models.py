e06f6a40c92dac480256c14c848fa0a0df53b550
Gabriel Santos Oliveira <santosgabriel28@hotmail.com>
2024-07-28 15:13:08 -0300

    Basic GNN model
    Since there are some ready-to-use models for GNNs and an MLP, I simply used them
    instead of defining my own by hand. From other tests, these work without any
    issues, but I have to read through the documentation to choose more parameters
    for selection.

--- src/models.py
+++ src/models.py
@@ -1,46 +1,49 @@
-from torch_geometric.nn.models import MLP, GCN
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCN, MLP
+from torch.utils import scatter
 
 
-class MyMLP():
-    def __init__(
-        self,
-        in_features: int,
-        hidden_features: int,
-        num_layers: int,
-        dropout: float,
-        act: str = 'relu',
-        norm: str = 'batch_norm',
-        bias: bool = True,
-    ):
-        return MLP(
-            in_channels=in_features,
-            out_channels=1,
-            hidden_channels=hidden_features,
-            num_layers=num_layers,
-            act=act,
-            norm=norm,
-            bias=bias)
+class MLPReadout(nn.Module):
+    """Layer for reading out the graph representation into the final regression
+    value
+    """
+
+    def __init__(self, input_dim, output_dim, L=2):
+        super.__init__()
+        layer_sizes = [input_dim//2**layer for layer in range(L)]
+        layer_sizes.append(1)
+        self.layers = MLP(layer_sizes, bias=True)
+
+    def forward(self, x):
+        return self.layers(x)
 
 
-class MyGCN():
-    def __init__(
-        self,
-        in_features: int,
-        hidden_features: int,
-        num_layers: int,
-        dropout: float,
-        act: str = 'relu',
-        bias: bool = True,
-    ):
-        return GCN(
-            in_channels=in_features,
-            out_channels=1,
+class Petri_GCN(nn.Module):
+    def __init__(self,
+                 in_features: int,
+                 h_dim: int,
+                 num_layers: int,
+                 dropout: float,
+                 act: str,
+                 norm: str):
+        self.GNN = GCN(
+            in_features=in_features,
+            hidden_channels=h_dim,
             num_layers=num_layers,
             dropout=dropout,
             act=act,
-            bias=bias,
-            normalize=False)
+            norm=norm)
+        self.MLP_layer = MLPReadout(self.h_dim, 1)
+        self.mae_loss = nn.L1Loss()
+        self.mre_loss = nn.MSELoss()
 
+    def forward(self, g):
+        x = self.GNN(g.x, g.edge_index)
+        x = self.MLP_layer(x)
+        return scatter(x, g.batch, dim=0, reduce='mean')
 
-class CNN():
-    pass
+    def loss(self, scores, targets):
+        loss = nn.L1Loss()(scores, targets)
+        return loss

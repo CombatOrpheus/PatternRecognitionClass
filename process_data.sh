51e7c96d1cc86397162ff62f8cc9c06193f5a67d
Gabriel Santos Oliveira <santosgabriel28@hotmail.com>
2024-07-28 15:18:29 -0300

    Minor fix
    I forgot to add the original file name to the new one, so I was overwriting most
    file.

--- process_data.sh
+++ process_data.sh
@@ -3,9 +3,10 @@ for file in $(find ./Data -name '*json'); do
 	name="$(basename -s .json $file)"
 	path=$(dirname "$file")
 	read -ra parts <<< "$(echo $file | awk -F/ '{print $3,  $4}')"
-	new_name="Data/${parts[0]}_${parts[1]}.processed"
+	new_name="Data/${parts[0]}_${parts[1]}_$name.processed"
 	set -x
-	jq -c '.[]' "$file" > "$new_name"
+	jq -c '.[]' "$file" > "$new_name" # &
 	{ set +x; } &> /dev/null
-	echo "File written to $new_name"
+	echo "File will be written to $new_name"
 done
+# wait

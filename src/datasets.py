b9ed43cceffd4fb0d6c28b2d9f3ef4a5352080d3
Gabriel Santos Oliveira <santosgabriel28@hotmail.com>
2024-07-29 06:36:31 -0300

    Padding node features
    The implementation seems correct, but I have not done thorough testing on it; a
    quick check shows that the usage of maps and generators is not to the liking of
    the DataLoader, so I might have to scale back to something like lists.

--- src/datasets.py
+++ src/datasets.py
@@ -1,3 +1,4 @@
+from copy import copy
 from itertools import starmap
 from pathlib import Path
 from typing import List, Iterable
@@ -11,8 +12,31 @@ from .data_parser import get_reachability_graphs
 
 
 def __pad_node_features__(data: Iterable):
-    pass
+    info = []
+    labels = []
+    for data, label in data:
+        info.append(data)
+        labels.append(label)
+    shapes = [np.shape(graph[0]) for graph in info]
+    max_size = max(x[1] for x in shapes)
+    sizes_to_pad = ((x[0], max_size - x[1]) for x in shapes)
+    padding = map(np.zeros, sizes_to_pad)
+    graphs = (x[0] for x in info)
+    padded_data = map(np.hstack, zip(graphs, padding))
+    new_info = [[padded_data, *graph[1:]] for graph in info]
+    return max_size, zip(new_info, labels)
 
+
+def __to_data__(
+    graph_info: List[np.array],
+    label: float
+) -> Data:
+    return Data(
+        x = as_tensor(graph_info[0]).float(),
+        edge_index=as_tensor(graph_info[1]).view(2, -1).long(),
+        edge_attr=as_tensor(graph_info[3][graph_info[2]]).float(),
+        y=as_tensor(label),
+        num_nodes=graph_info[0].shape[0])
 
 def __to_data_reduced_features__(
         graph_info: List[np.array],
@@ -32,9 +56,12 @@ def get_reachability_dataset(
     batch_size=16
 ) -> DataLoader:
     data = get_reachability_graphs(source)
-    if reduce_node_features:
-        data_iterator = list(starmap(__to_data_reduced_features__, data))
-    else:
-        data_iterator = __pad_node_features__(data)
-
-    return DataLoader(data_iterator, batch_size=batch_size)
+    f = __to_data_reduced_features__ if reduce_node_features else __to_data__
+    if not reduce_node_features:
+        size, data = __pad_node_features__(data)
+        
+    data_iterator = list(starmap(f, data))
+    loader = DataLoader(data_iterator, batch_size=batch_size)
+    loader.num_features = 1 if reduce_node_features else size
+    
+    return loader

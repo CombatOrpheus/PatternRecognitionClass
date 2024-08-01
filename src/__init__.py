c25805338e35b1e17de2117d99f7ff0716d737ba
Gabriel Santos Oliveira <santosgabriel28@hotmail.com>
2024-08-01 05:50:09 -0300

    fixing imports with isort

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 from .data_parser import get_data, get_petri_nets, get_reachability_graphs
-from .petri_characteristics import *
 from .datasets import get_reachability_dataset
-from .models import Petri_GCN, Petri_Cheb_GNN
+from .models import Petri_Cheb_GNN, Petri_GCN
+from .petri_characteristics import *

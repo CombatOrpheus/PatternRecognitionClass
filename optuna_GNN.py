b5b80ed364a6723b524e54b479374b0df7d55e20
Gabriel Santos Oliveira <santosgabriel28@hotmail.com>
2024-08-08 11:24:22 -0300

    removing unused imports, simplifying script
    Isolating training into it's own function helps a lot with organization. On other news, I have finally included the validation/testing step to the optimization process and removed a lot of unused variables/imports.

--- optuna_GNN.py
+++ optuna_GNN.py
@@ -1,59 +1,76 @@
-import os
 from pathlib import Path
 
 import optuna
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.data
 from optuna.trial import TrialState
-from tqdm import tqdm, trange
+from tqdm import trange
 
 from src.datasets import get_reachability_dataset
 from src.models import Petri_GCN
 
-DEVICE = torch.device("cpu")
-CLASSES = 10
-DIR = os.getcwd()
+DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 EPOCHS = 100
+MAE = True
 
 
 def define_model(trial, num_features):
     return Petri_GCN(
         in_channels=num_features,
-        hidden_features=trial.suggest_int("Hidden Features", 16, 64, step=2),
-        num_layers=trial.suggest_int("Number of GCN Layers", 2, 10),
-        dropout=trial.suggest_float("Dropout", 0.0, 0.2, step=1e-3),
-        ################################################################
-        # act=trial.suggest_categorical(                               #
-        #     "Activation Function",                                   #
-        #     ["ReLU", "LeakyReLU", "Sigmoid", "Softmin", "Softmax"]), #
-        # norm=trial.suggest_categorical(                              #
-        #     "Normalization Function",                                #
-        #     ["GraphNorm", "LayerNorm", "BatchNorm"]),                #
-        ################################################################
-        readout_layers=trial.suggest_int("MLP Readout Layers", 2, 4)
+        hidden_features=trial.suggest_int("Hidden Features", 2, 16, step=2),
+        num_layers=trial.suggest_int("Number of GCN Layers", 2, 20),
+        readout_layers=trial.suggest_int("MLP Readout Layers", 2, 5),
+        mae=MAE
         )
+
+
+def train_model(model, optimizer, train_loader):
+    model.train()
+    for epoch in trange(EPOCHS, leave=False, desc="Training loop"):
+        for graph in train_loader:
+            data, target = graph, graph.y
+            optimizer.zero_grad()
+            output = torch.flatten(model(data))
+            loss = model.loss(output, target)
+            loss.backward()
+            optimizer.step()
+    return loss
+
+
+def eval_model(model, valid_loader):
+    model.eval()
+    pred = []
+    actual = torch.tensor([graph.y for graph in valid_loader.dataset])
+    for graph in valid_loader:
+        pred.extend(model(graph).tolist())
+
+    pred = torch.tensor(pred)
+    pred = torch.flatten(pred)
+
+    return model.loss(pred, actual)
 
 
 def get_data(trial):
     train_data = Path('Data/RandData_DS1_train_data.processed')
     test_data = Path('Data/RandData_DS1_test_data.processed')
 
+    batch_size = trial.suggest_int("Batch Size", 16, 128, step=4)
+
     train_dataset = get_reachability_dataset(
         train_data,
         reduce_node_features=True,
-        batch_size=64)
+        batch_size=batch_size)
+
     test_dataset = get_reachability_dataset(
         test_data,
         reduce_node_features=True,
-        batch_size=64)
+        batch_size=batch_size)
     return train_dataset, test_dataset
 
 
 def objective(trial):
-    train_loader, valid_loader = get_data(trial)
+    train_loader, test_loader = get_data(trial)
     # Generate the model.
     model = define_model(trial, train_loader.num_features).to(DEVICE)
 
@@ -65,21 +82,15 @@ def objective(trial):
     optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=lr)
 
     # Training of the model.
-    model.train()
-    for epoch in trange(EPOCHS, leave=False, desc="Training loop"):
-        for graph in tqdm(train_loader, leave=False):
-            data, target = graph, graph.y
-            optimizer.zero_grad()
-            output = torch.flatten(model(data))
-            loss = F.l1_loss(output, target)
-            loss.backward()
-            optimizer.step()
-
+    for epoch in trange(30):
+        loss = train_model(model, optimizer, train_loader)
         trial.report(loss, epoch)
-
         # Handle pruning based on the intermediate value.
         if trial.should_prune():
             raise optuna.exceptions.TrialPruned()
+
+    loss = eval_model(model, test_loader)
+    trial.report(loss, epoch)
 
     return loss
 
@@ -91,10 +102,10 @@ if __name__ == "__main__":
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
 
-    print("Study statistics: ")
-    print("  Number of finished trials: ", len(study.trials))
-    print("  Number of pruned trials: ", len(pruned_trials))
-    print("  Number of complete trials: ", len(complete_trials))
+    print(f"""Study statistics:
+                Number of finished trials: ", {len(study.trials)}
+                Number of pruned trials: ", {len(pruned_trials)}
+                Number of complete trials: ", {len(complete_trials)}""")
 
     print("Best trial:")
     trial = study.best_trial

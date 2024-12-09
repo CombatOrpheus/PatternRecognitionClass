16f7f33eafba3b7b745f3e4b1984ba5a9db9d40b
Gabriel Santos Oliveira <santosgabriel28@hotmail.com>
2024-12-09 19:49:51 -0300

    Basic analysis
    The basic model evaluation is ready. Other functions of interest
    are for evaluating the model against all available datasets and
    creating some plots/tables with the results.

--- src/analysis.py
+++ src/analysis.py
@@ -1,26 +1,38 @@
 from pathlib import Path
-from typing import Iterable
 
 import numpy as np
 import torch
 from torch.nn import Module
+from torch.nn import functional as F
+from torch_geometric.loader import DataLoader
 
 from .data_parser import get_average_tokens
 
 
 def variance(sample) -> torch.tensor:
-    return torch.mean((sample - torch.mean(sample))**2)
+    return torch.mean((sample - torch.mean(sample)) ** 2)
 
 
 def std_deviation(sample) -> torch.tensor:
-    return variance(sample)**2
+    return variance(sample) ** 2
 
 
 def statistical_values(sample) -> torch.tensor:
     return torch.mean(sample), std_deviation(sample), variance(sample)
 
 
+def evaluate_model(model: Module, dataset: DataLoader) -> np.array:
+    actual = torch.tensor([batch.y for batch in dataset])
+    pred = list(map(model, dataset))
+
+    pred = torch.tensor(pred)
+    pred = torch.flatten(pred)
+
+    mre = torch.mean(torch.abs(pred - actual) / actual) * 100
+    return F.l1_loss(pred, actual), torch.nn.MSELoss()(pred, actual), mre
+
+
 def evaluate_metrics(model: Module, path: Path) -> np.array:
-    files = map(get_average_tokens, path.glob('*all*'))
+    files = map(get_average_tokens, path.glob("*all*"))
     for dataset in files:
         pass
